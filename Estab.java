d7bb1d895997af2ea58c9f77004e9c147e97f3d8
Johnny P test <jphilavanh@gmail.com>
2015-07-19 16:06:39 -0700

    Create Estab.java

---
+++ Estab.java
@@ -0,0 +1,17 @@
+/*
+Johnny Philavanh
+Estab.java
+ */
+class Estab
+{
+public String controls; //name of control sections
+public String symn; //symbol name
+public String addy; //address
+
+    	public Estab(String controls, String symn, String addy)
+    	{
+    	this.controls = controls;
+    	this.symn = symn;
+    	this.addy = addy;
+    	}
+}
